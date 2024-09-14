bd1024a32f6ad29ab89af932edc07d784a79f6e4
AjolotitoDev <164126382+AjolozulXD@users.noreply.github.com>
2024-09-13 22:23:02 -0300

    Updated_Bot.py

--- Bot.py
+++ Bot.py
@@ -1,34 +1,48 @@
 import discord
 import settings
-from bot_logic import gen_pass
-from bot_logic import flip_coin
+from bot_logic import gen_pass, flip_coin
+from discord.ext import commands
+import random
+
 
 # La variable intents almacena los privilegios del bot
 intents = discord.Intents.default()
 # Activar el privilegio de lectura de mensajes
 intents.message_content = True
-# Crear un bot en la variable cliente y transferirle los privilegios
-client = discord.Client(intents=intents)
+# Crear un bot en la variable bot y transferirle los privilegios
+bot = commands.Bot(command_prefix="B", intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    print(f'Hemos iniciado sesión como {client.user}')
+    print(f'Hemos iniciado sesión como {bot.user}')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hola'):
-        await message.channel.send("Holas")
-    elif message.content.startswith('$chau'):
-        await message.channel.send("Chau, hermos@!")
-    elif message.content.startswith('$pass'):
-        await message.channel.send(gen_pass (10))
-    elif message.content.startswith('$coin'):
-        await message.channel.send(flip_coin())
-    else:
-        await message.channel.send("No puedo procesar este comando, ¡lo siento!")
+@bot.command()
+async def hola(ctx):
+    await ctx.send("holis")
     
-client.run("TÚ TOKEN")
+@bot.command()
+async def chau(ctx):
+    await ctx.send("Adiós, hermos@")
 
-client.run(settings["TÚ TOKEN"])
+@bot.command()
+async def password(ctx):
+    await ctx.send(gen_pass(10))
+
+@bot.command()
+async def coind(ctx):
+    await ctx.send(flip_coin())
+
+@bot.command()
+async def roll(ctx, dice: str):
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+    
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
+    
+bot.run("TÚ TOKEN")
+
+bot.run(settings["TÚ TOKEN"])
